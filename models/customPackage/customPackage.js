9ab3fc1472d1b49cc27e8052f612ad988c5c4d14
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2024-02-23 00:50:55 +0500

    custom package model created

---
+++ models/customPackage/customPackage.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const customPackage = mongoose.Schema({
+  numberOfAdults: {
+    type: Number,
+    required: true,
+  },
+  numberOfKids: {
+    type: Number,
+    required: false,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  totalExpectedTripDays: {
+    type: Number,
+    required: true,
+  },
+  customMessage: {
+    type: String,
+    required: true,
+  },
+  chooseVehicle: {
+    type: String,
+    required: true,
+  },
+  needHotel: {
+    type: Boolean,
+    required: true,
+  },
+  hotelBudget: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("CustomPackage", customPackage);
