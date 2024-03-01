05b6f45b23d386c1a58785a66eeb6c77b0842ad6
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2024-03-01 06:06:10 +0500

    custom booking create controller created

---
+++ controllers/customBooking/create.js
@@ -0,0 +1,47 @@
+const CustomBooking = require("../../models/customBooking");
+
+module.exports = async (req, res, next) => {
+  const {
+    numberOfAdult,
+    numberOfKids,
+    startDate,
+    endDate,
+    numberofDays,
+    needHotel,
+    hotelBudget,
+    pickup,
+    customMessage,
+    status,
+  } = req.body;
+  try {
+    if (
+      !numberOfAdult ||
+      !numberOfKids ||
+      !startDate ||
+      !endDate ||
+      !numberofDays ||
+      !needHotel ||
+      !hotelBudget ||
+      !pickup ||
+      !customMessage
+    )
+      return res.status(403).json({ message: "Incomplete payload" });
+
+    const newCustomBooking = new CustomBooking({
+      numberOfAdult,
+      numberOfKids,
+      startDate,
+      endDate,
+      numberofDays,
+      needHotel,
+      hotelBudget,
+      pickup,
+      customMessage,
+      status,
+    });
+    await newCustomBooking.save();
+    return res.status(200).json(newCustomBooking);
+  } catch (e) {
+    return console.log("Error creating custom booking");
+  }
+};
