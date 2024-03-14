3aac7363a9208b22fe261042bc4df2584bf79d1a
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2024-03-14 23:22:46 +0500

    custom booking crud created

---
+++ controllers/customBooking/update.js
@@ -0,0 +1,47 @@
+const CustomBooking = require("../../models/customBooking");
+
+module.exports = async (req, res, next) => {
+  const { bookingId } = req.params;
+  const {
+    numberOfAdult,
+    numberOfKids,
+    startDate,
+    endDate,
+    numberofDays,
+    needHotel,
+    hotelBudget,
+    pickup,
+    customMessage,
+    status,
+  } = req.body;
+
+  try {
+    if (!bookingId) {
+      return res.status(400).json({ message: "Booking ID is required" });
+    }
+
+    let customBooking = await CustomBooking.findById(bookingId);
+
+    if (!customBooking) {
+      return res.status(404).json({ message: "Custom booking not found" });
+    }
+
+    customBooking.numberOfAdult = numberOfAdult;
+    customBooking.numberOfKids = numberOfKids;
+    customBooking.startDate = startDate;
+    customBooking.endDate = endDate;
+    customBooking.numberofDays = numberofDays;
+    customBooking.needHotel = needHotel;
+    customBooking.hotelBudget = hotelBudget;
+    customBooking.pickup = pickup;
+    customBooking.customMessage = customMessage;
+    customBooking.status = status;
+
+    customBooking = await customBooking.save();
+    c;
+    return res.status(200).json(customBooking);
+  } catch (error) {
+    console.error("Error updating custom booking:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
