b88f5276c5eeef37e72606bdd9fb094ee9bd4a2c
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2024-03-14 23:59:26 +0500

    per day booking crud created

---
+++ controllers/perDayBooking/getAll.js
@@ -0,0 +1,16 @@
+const PerDayBooking = require("../../models/perDayBooking");
+
+module.exports = async (req, res, next) => {
+  try {
+    const perDayBookings = await PerDayBooking.find();
+
+    if (perDayBookings.length === 0) {
+      return res.status(404).json({ message: "No per-day bookings found" });
+    }
+
+    return res.status(200).json(perDayBookings);
+  } catch (error) {
+    console.error("Error fetching per-day bookings:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
