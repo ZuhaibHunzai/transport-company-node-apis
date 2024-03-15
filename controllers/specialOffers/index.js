8adbb546109bbb38089fcce93a3f0d9bba84b26e
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2024-03-16 00:24:52 +0500

    special offers routes created

---
+++ controllers/specialOffers/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  createSpecialOffer: require("./create"),
+  deleteSpecialOffer: require("./delete"),
+  getAllSpecialOffers: require("./getAll"),
+  getSingleSpecialOffer: require("./getSingle"),
+  updateSpecialOffer: require("./update"),
+};
---
+++ routes/specialOffers.js
@@ -0,0 +1,17 @@
+const specialOffersRoutes = requre("express").Router();
+
+const useAuth = require("../middleware/useAuth");
+const {
+  createSpecialOffer,
+  deleteSpecialOffer,
+  getAllSpecialOffers,
+  getSingleSpecialOffer,
+  updateSpecialOffer,
+} = require("../controllers/specialOffers");
+
+specialOffersRoutes
+  .post("/add", useAuth, createSpecialOffer)
+  .delete(":/id", useAuth, deleteSpecialOffer)
+  .get("/all", getAllSpecialOffers)
+  .get(":/id", getSingleSpecialOffer)
+  .post("/update", useAuth, updateSpecialOffer);
